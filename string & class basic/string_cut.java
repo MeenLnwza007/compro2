22900db5d15824916c5208c5c2c4bab5bb7d79a5
Rian dec <116154727+MeenLnwza007@users.noreply.github.com>
2024-05-15 18:33:56 +0700

    Add files via upload

--- string & class basic/string_cut.java
+++ string & class basic/string_cut.java
@@ -5,28 +5,26 @@ public class string_cut {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
-        char alphabet = sc.next().charAt(0);
+        String str = sc.nextLine();
+        char c = sc.next().charAt(0);
+        int index = str.indexOf(c);
+        int count = 0;
         boolean found = false;
-        boolean print = false;
-        int prev = 0;
-        for(int i=0;i<text.length();i++){
-            if(text.charAt(i) == alphabet){
-                found = true;
-                if(prev != i){
-                    System.out.println(text.substring(prev, i));
-                    print = true;
-                }
-                prev = i+1;
-            } else if(i == text.length()-1 && prev != 0){
-                System.out.println(text.substring(prev, text.length()));
-                print = true;
-            }
-        }
-
-        if(!found){
+        if(index == -1){
             System.out.println("no target character found");
-        } else if(!print){
+            return;
+        }
+        while(index != -1){
+            String output = str.substring(0, index);
+            if(output.length() > 0){
+                System.out.println(output);
+                count++;
+            }
+            str = str.substring(index+1);
+            index = str.indexOf(c);
+        }
+        System.out.print(str);
+        if(count == 0){
             System.out.println("no output");
         }
     }
